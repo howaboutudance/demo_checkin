de563d156416d8cb83912c8bde88b7e7c9d0a716
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-15 15:10:24 -0700

    db file

---
+++ app/db.py
@@ -0,0 +1,15 @@
+import psycopg2 as pg
+
+from flask import current_app. g
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(get_db)
+
+def get_db():
+    if 'db' not in g:
+        g.db = pg.connect(dbname = "crimson-dev", user="checkincl", password="clrocks59")
+    return g.db
+def close_db(error=None):
+    if hasattr(g, 'db'):
+        g.db.close()
