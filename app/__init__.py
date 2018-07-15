2f9156f1cf9e19035eb65dd6b1c8f50bd0e8e893
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-15 15:21:16 -0700

    brought back in old routes

--- app/__init__.py
+++ app/__init__.py
@@ -35,4 +35,40 @@ def create_app(test_config=None):
     @app.route('/index')
     def index():
         return "Welcome to checkin1"
+    @app.route('/api/v1.0/students', methods=['GET'])
+    def get_students():
+        return jsonify({"students": students})
+
+    @app.route('/api/v1.0/student/<string:anum>', methods=['GET'])
+    def get_student(anum):
+        student = [student for student in students if student['anum'] == anum]
+        if len(student) == 0:
+            abort(404)
+        return jsonify({"student":student[0]})
+
+    @app.route('/api/v1.0/student/', methods=['POST'])
+    def set_student():
+        if not request.json or not 'anum' in request.json:
+            abort(400)
+
+        student = {
+                'anum':request.json['anum'], 
+                'firstName':request.json['firstName'], 
+                'lastName':request.json['lastName']
+        }
+
+
+        students.append(student)
+        return jsonify({'student':student}), 201
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"error": 'not found'})
+
     return app
+def matchfield(req, record):
+    def assignlmb(fi, v):
+        record[fi] = v
+        return fi
+    updated = [assignlmb(fi, req[fi]) for fi in req.keys()]
+    return updated
