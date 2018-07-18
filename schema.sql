972b6f08f207f36edd77ab680c712ef1795f4441
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-18 13:13:33 -0700

    added schema for project -- update as needed
    schema.sql
    ----------
    
    sql queries to create database

---
+++ schema.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS preferredname;
+DROP TABLE IF EXISTS session;
+DROP TABLE IF EXISTS studentsession;
+DROP TABLE IF EXISTS faculty;
+DROP TABLE IF EXISTS facultyteam;
+
+DROP TYPE IF EXISTS session_kind;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(40) UNIQUE NOT NULL,
+	password VARCHAR(128) NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS student(
+       anum CHAR(9) NOT NULL PRIMARY KEY,
+       firstname VARCHAR(64),
+       lastname VARCHAR(64)
+); 
+
+CREATE TABLE IF NOT EXISTS preferredname(
+      anum CHAR(9) REFERENCES student(anum),
+      preferredfirstname VARCHAR(64),
+      pronouns VARCHAR(24)
+);
+
+CREATE TYPE session_kind AS ENUM('mandatory', 'affinity', 'c2c', 'seminar');
+CREATE TABLE IF NOT EXISTS session(
+       id SERIAL NOT NULL PRIMARY KEY,
+       name VARCHAR(128),
+       seats INT,
+       location VARCHAR(128),
+       starttime TIMESTAMPTZ,
+       length FLOAT,
+       kind session_kind
+);
+
+CREATE TABLE IF NOT EXISTS studentsession(
+       id SERIAL NOT NULL PRIMARY KEY,
+       anum char(9) NOT NULL,
+       session_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS faculty (
+       id SERIAL NOT NULL PRIMARY KEY,
+       name VARCHAR(128)
+);
+CREATE TABLE IF NOT EXISTS facultyteam (
+       id SERIAL NOT NULL PRIMARY KEY,
+       session_id INT,
+       faculty_id INT
+);
+COMMIT;
+
