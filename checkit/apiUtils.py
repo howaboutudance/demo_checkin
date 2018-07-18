efadee051a68c0ccdd91ed5a708470d9cecb072c
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-18 14:33:27 -0700

    refactored out api to sub urls to seperate blueprint modules

---
+++ checkit/apiUtils.py
@@ -0,0 +1,37 @@
+from flask import jsonify, abort
+
+#errorhandlers
+@bp.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": '404', "message": "file not found"}), 404
+
+@bp.errorhandler(405)
+def not_allowed(error):
+    return jsonify({"error":"405", "message": "method not allowed"}), 405
+
+#helper functions
+def matchfield(req, record):
+    def assignlmb(fi, v):
+        record[fi] = v
+        return fi
+    updated = [assignlmb(fi, req[fi]) for fi in req.keys()]
+    
+    return updated
+
+def tag_one(entity, fields, rec):
+    if not rec:
+        return(jsonify({"error":"404", "message":"no record found"}), 404)
+
+    reponse = jsonify({entity: dict(zip(fields, rec))})
+    return(response, 200)
+
+def tag_many(entity, fields, recs, message="record not found"):
+    if len(recs) == 0:
+        return (jsonify({"error":"404", "message":message}),404)
+
+    response = jsonify({entity:[dict(zip(fields, x)) for x in recs]})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return(response, 200)
+
+queryify = lambda x:{True: "'{0}'".format(x), False:str(x)}[type(x)==str]
+qiy = lambda x:"{0} = {1}".format(str.lower(x[0]), queryify(x[1]))
