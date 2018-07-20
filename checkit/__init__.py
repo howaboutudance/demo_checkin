e8e716f897db9417f1fb3c65fe1311051b49ba85
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-19 22:05:39 -0700

    fixed development database in __init__.py
    __init__.py
    -----------
    
    added "crimson-dev" to the database URI

--- checkit/__init__.py
+++ checkit/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask (__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-            DATABASE = 'postgresql://checkincl:clrocks59@127.0.0.1:5432',
+            DATABASE = 'postgresql://checkincl:clrocks59@127.0.0.1:5432/crimson-dev',
             SECRET_key = 'dev')
 
     if test_config is None:
