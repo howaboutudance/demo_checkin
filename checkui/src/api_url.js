91e1e148df7f19aea61cb657ee5a238036e963fa
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-24 15:53:24 -0700

    ref #6 refacore out api_base_url into api_url.js

---
+++ checkui/src/api_url.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const host = "localhost"
+const port = "5000"
+const api_version = "1.0"
+const api_base_url = `http:////${host}:${port}/api/v${api_version}`
+
+export {api_base_url}
