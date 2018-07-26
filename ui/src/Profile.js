71e5e3ae92fa0468a3de3602e2db19ed4f7cc54c
Michael Penhallegon <howaboutudance@gmail.com>
2018-07-25 23:19:02 -0700

    implemented a form layout ref #16 @1h20
    implemented a form layout with react-jsonschema-form
    
    ui/src/Profile.js
    -----------------
    - added ProfileForm
    - added static json scheam
    - changed from textarea to form

--- ui/src/Profile.js
+++ ui/src/Profile.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './demo.css';
+import Form from "react-jsonschema-form"
 
+const schema = {
+  title: "Student Details",
+  type: "object",
+  required: ["title"],
+  properties: {
+    firstName: {type: "string", title: "First Name", default: "First"},
+    lastName: {type: "string", title: "Last Name", default: "Last"},
+	preferredFirstName: {type:"string", title:"Preferred Name", default:"Preferred"},
+	pronoun: {type: "string", title: "Pronoun", default:"Pronoun"}
+  }
+}
 class Profile extends React.Component {
 	constructor(props) {
 		super(props);
@@ -13,9 +25,24 @@ class Profile extends React.Component {
 	render() { 
 		return(
 			<div className="profile">
-				<textarea value={this.props.query}>
-				</textarea>
-			</div>)}
+				<ProfileForm query={this.props.query} />
+			</div>
+		)}
 }
+
+class ProfileForm extends React.Component {
+	render () {
+		return (
+			<Form schema = {schema}
+				formData = {this.props.query}
+				onchange={console.log("changed")}
+				onSubmit={console.log("submitted")}
+				onError = {console.error("errors")}>
+				<div><button type="submit">Update</button></div>
+			</Form>
+		)
+	}
+}
+
 
 export { Profile } 
